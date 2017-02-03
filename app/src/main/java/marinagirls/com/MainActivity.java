6abd89115c19e77169a5ed50bb850728d31f841d
David Dayer <dayerdl@gmail.com>
2017-02-03 19:56:59 +0400

    Added login with Facebook and Firebase

--- app/src/main/java/marinagirls/com/MainActivity.java
+++ app/src/main/java/marinagirls/com/MainActivity.java
@@ -1,27 +1,42 @@
 package marinagirls.com;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import marinagirls.com.activities.FacebookLoginActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TRIPS_FRAGMENT = "Trips";
-    private static final String ADD_TRIP_FRAGMENT = "Add";
-    private static final String MY_TRIPS_FRAGMENT = "MyTrips";
+    private static final String ADD_TRIP_FRAGMENT = "Add trip";
+    private static final String MY_TRIPS_FRAGMENT = "My trips";
     BottomNavigationView bottomNavigationView;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupViews();
+        setupToolbar();
         addFragment(TRIPS_FRAGMENT);
 
     }
+
+    private void setupToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(mToolbar);
+    }
+
+
 
     private void setupViews() {
         bottomNavigationView = (BottomNavigationView)
@@ -47,22 +62,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addFragment(String tripsFragment) {
+        mToolbar.setTitle(tripsFragment);
         if(tripsFragment.equals(TRIPS_FRAGMENT)) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.containter, TripsFragment.newInstance(), tripsFragment)
                     .commit();
         } else if(tripsFragment.equals(ADD_TRIP_FRAGMENT)){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.containter, AddTripFragment.newInstance(), tripsFragment)
-                    .commit();
+            checkLogin();
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.containter, AddTripFragment.newInstance(), tripsFragment)
+//                    .commit();
         } else if(tripsFragment.equals(MY_TRIPS_FRAGMENT)){
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.containter, MyTripsFragment.newInstance(), tripsFragment)
                     .commit();
         }
+    }
+
+    private void checkLogin() {
+        Intent i = new Intent(this, FacebookLoginActivity.class);
+        startActivity(i);
+
     }
 
 
