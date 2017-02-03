76d46d5b08c2b1b1f039739536fc519cac50a304
David Dayer <dayerdl@gmail.com>
2017-02-03 15:35:45 +0400

    Added android app

---
+++ app/src/main/java/marinagirls/com/MainActivity.java
@@ -0,0 +1,48 @@
+package marinagirls.com;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupViews();
+    }
+
+    private void setupViews() {
+        bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_trips:
+
+                                break;
+                            case R.id.action_schedules:
+
+                                break;
+                            case R.id.action_music:
+
+                                break;
+                        }
+                        return false;
+                    }
+                });
+    }
+
+
+}
